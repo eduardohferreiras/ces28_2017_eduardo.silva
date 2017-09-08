d7214aae3ab874eb144ea4a4b19a821e8f5d9f8b
Eduardo Henrique <eduardo.hferreiras@gmail.com>
2017-09-08 17:11:49 -0300

    Adição de mais alguns casos de teste.

--- labs/lab02/src/lab02/CalculadoraString.java
+++ labs/lab02/src/lab02/CalculadoraString.java
@@ -1,18 +1,41 @@
 package lab02;
 
+import java.util.Arrays;
+
 public class CalculadoraString {
 
 	static public int add(String numbers) {
+		int index = 0;
+		int sum = 0;
+		String regex = null;
+		
 		if(numbers.length() == 0)
 			return 0;
 		
-		int sum =0;
-		String array[] = numbers.split(",");
+		String array[] = numbers.split("]\n");
 		
+		if(array.length == 1)
+		{
+			index = 0;
+			regex = " *,+ *| *\\n+ *";
+		}
+		else if (array.length == 2)
+		{
+			index = 1;
+			char delimiter = array[0].charAt(3);
+			regex = " *,+ *| *\n+ *| *" + delimiter + "+ *";
+		}
+		
+	
+		array = array[index].split(regex);
 		for(int i = 0; i< array.length; i++)
 		{
-			sum += Integer.parseInt(array[i]);
+			if(!array[i].isEmpty() && array[i]!= " ") {
+				sum += Integer.parseInt(array[i]);
+			}
 		}
 		return sum;
+
 	}
+
 }
--- labs/lab02/tests/lab02/unitTests.java
+++ labs/lab02/tests/lab02/unitTests.java
@@ -7,23 +7,50 @@ import org.junit.Test;
 
 public class unitTests {
 	private String input;
-	
-	@Before
-	public void setup()
-	{
-	}
-		
-	
+
 	@Test
 	public void whenTheSeparatorIsCommaAndTheOnlyInvalidCharIsSpaceReturnsTheSum()
-	{
+	{	
 		input = "";
 		assertEquals(0, CalculadoraString.add(input));
 		input = "1";
 		assertEquals(1, CalculadoraString.add(input));
 		input = "1,2,3";
 		assertEquals(6, CalculadoraString.add(input));
-		input = "1, 2,3";
+		input = "1   ,      2,3";
 		assertEquals(6, CalculadoraString.add(input));
 	}
+	@Test
+	public void whenThereAreALotOfSpacesAndCommasTheMethodWillIgnoreThoseChars()
+	{
+		input = "  , , ,     ,  ,";
+		assertEquals(0, CalculadoraString.add(input));
+		input = ",,,,  ,,  ,,,,";
+		assertEquals(0, CalculadoraString.add(input));
+		input = "1,2,3,  , ,,  ,4";
+		assertEquals(10, CalculadoraString.add(input));
+		input = " ";
+		assertEquals(0, CalculadoraString.add(input));
+	}
+
+	@Test
+	public void newLineCharCanBeASeparatorToo()
+	{
+		input = "1\n2,3";
+		assertEquals(6, CalculadoraString.add(input));
+		input = "1,\n";
+		assertEquals(1, CalculadoraString.add(input));
+	}	
+	
+	@Test
+	public void newCustomSeparatorsAreAccepted()
+	{
+		input = "//[;]\n1;2,3\n4";
+		assertEquals(10, CalculadoraString.add(input));
+		input = "//[;]\n\n\n\n1,;,2,,, ,  ; ; ;;;3\n4";
+		assertEquals(10, CalculadoraString.add(input));
+		
+	}
+	
+	//
 }
