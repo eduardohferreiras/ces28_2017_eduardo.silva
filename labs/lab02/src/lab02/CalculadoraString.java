a1e395f26119e8e613d866668d61f9324d17493c
Felipe <guima.felipec@gmail.com>
2017-09-16 17:22:10 -0300

    Lab terminado

--- labs/lab02/src/lab02/CalculadoraString.java
+++ labs/lab02/src/lab02/CalculadoraString.java
@@ -12,20 +12,36 @@ public class CalculadoraString {
 		if(numbers.length() == 0)
 			return 0;
 		
-		String array[] = numbers.split("]\n");
+		String array[] = numbers.split("]|]\n");
 		
 		if(array.length == 1)
 		{
 			index = 0;
 			regex = " *,+ *| *\\n+ *";
 		}
-		else if (array.length == 2)
+		else if (array.length >= 2)
 		{
 			try {
-				index = 1;
-				String delimiter = array[0].charAt(3) + "";
-				String delimiterEscaped = Pattern.quote(delimiter);
-				regex = " *,+ *| *\n+ *| *" + delimiterEscaped + "+ *";
+				index = array.length - 1;
+				regex = " *,+ *| *\n+ *| *";
+				for(int j = 0; j < index; j++) {
+					
+					int i = array[j].length() - 1;
+					char atual = array[j].charAt(i);
+					if(atual == '[')
+						throw new IllegalArgumentException();
+					String delimiter = "";
+					while(atual != '[') {	
+						delimiter = delimiter + atual;
+						atual = array[j].charAt(--i);
+					}
+					
+					//String delimiter = array[0].charAt(3) + "";
+					String delimiterEscaped = Pattern.quote(delimiter);
+					regex = regex + delimiterEscaped + "+ *";
+					if(j < index -1)
+						regex = regex + "|";
+				}
 			}
 			catch(IndexOutOfBoundsException exception)
 			{
@@ -43,7 +59,7 @@ public class CalculadoraString {
 		for(int i = 0; i< array.length; i++)
 		{
 
-			if(!array[i].isEmpty() && array[i]!= " " && Integer.parseInt(array[i]) >= 0 ) {
+			if(!array[i].isEmpty() && array[i]!= " " && (Integer.parseInt(array[i]) >= 0 && Integer.parseInt(array[i]) < 1000) ) {
 				sum += Integer.parseInt(array[i]);
 			}
 			else if(!array[i].isEmpty() && array[i]!= " " && Integer.parseInt(array[i]) < 0 )
--- labs/lab02/tests/lab02/unitTests.java
+++ labs/lab02/tests/lab02/unitTests.java
@@ -5,7 +5,7 @@ import static org.junit.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-public class unitTests {
+public class unitTests2 {
 	private String input;
 
 	@Test
